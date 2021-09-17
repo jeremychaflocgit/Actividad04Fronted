7270de1b25b78872b34308e163dd1a8acd615524
jeremychaflocgit <i201916552@cibertec.edu.pe>
2021-09-17 13:20:56 -0500

    first commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Semana03'),
+      dir: require('path').join(__dirname, './coverage/Actividad04'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 
 
 export class AppComponent {
-  title = 'Semana03';
+  title = 'Actividad04';
 
   constructor(http: HttpClient ){
 
